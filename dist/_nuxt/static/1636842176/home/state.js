8d9b1183d695abd781fe31718f4ac06e7e75e34a
Miguel Luis Hernandez Brocat <mhernandezbrocat@gmail.com>
2021-11-13 13:56:31 -0800

    Add files via upload

---
+++ dist/_nuxt/static/1636842176/home/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fappmovie\u002F_nuxt\u002Fstatic\u002F1636842176",layout:"movieapp",error:a,serverRendered:true,routePath:"\u002Fhome",config:{_app:{basePath:"\u002Fappmovie\u002F",assetsPath:"\u002Fappmovie\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ca9e03ad"}}}}(null));
